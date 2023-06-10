f385da33c197935132daffa61c48d2e9ffb50534
Hatamleh <hatem.hatamleh@gmail.com>
2023-06-10 23:29:01 +0200

    add host allure hosting

--- src/test/java/com/qacart/todo/factory/DriverFactory.java
+++ src/test/java/com/qacart/todo/factory/DriverFactory.java
@@ -2,6 +2,7 @@ package com.qacart.todo.factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
@@ -14,7 +15,9 @@ public class DriverFactory {
         String browser = System.getProperty("browser", "CHROME");
         switch (browser) {
             case "CHROME" -> {
-                driver = new ChromeDriver();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless");
+                driver = new ChromeDriver(options);
             }
             case "FIREFOX" -> {
                 driver = new FirefoxDriver();
